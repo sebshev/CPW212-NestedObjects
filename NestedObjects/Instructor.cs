63f971fd5445f7c2ee0f37f1e89e9a101745b7ea
Sebastian Shevlin <seb112697@gmail.com>
2021-01-23 16:27:51 -0800

    Added Student and Instructor classes

---
+++ NestedObjects/Instructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    class Instructor
+    {
+        /// <summary>
+        /// The instructor's full legal name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Company email for instructor
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
